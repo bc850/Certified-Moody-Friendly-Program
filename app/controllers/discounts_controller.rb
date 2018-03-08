11abf0809037ee2ac307d92d661058298e649620
amandavsu2018 <amandavsu2018@gmail.com>
2018-03-08 15:38:56 -0500

    updated views

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -69,6 +69,6 @@ class DiscountsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def discount_params
-      params.require(:discount).permit(:name, :start_date, :end_date, :category)
+      params.require(:discount).permit(:name, :start_date, :end_date, :category, :description, :location)
     end
 end
