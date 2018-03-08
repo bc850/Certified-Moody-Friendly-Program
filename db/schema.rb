3b2e2f707e6dd2cf65f06bb953686b28fc448acb
amandavsu2018 <amandavsu2018@gmail.com>
2018-03-08 15:26:08 -0500

    adding desc and location columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308194446) do
+ActiveRecord::Schema.define(version: 20180308201733) do
 
   create_table "discounts", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20180308194446) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.text "location"
   end
 
 end
