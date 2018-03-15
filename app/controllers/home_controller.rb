3ec72b8b209751e8562a3f4cf718ed33ff25a6d3
Brandon Corbett <bmcorbet@valdosta.edu>
2018-03-14 20:04:36 -0400

    created combined coupons and discounts feed, home controller has sorting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,8 @@ class HomeController < ApplicationController
   def index
     @discounts = Discount.all
     @coupons = Coupon.all
+
+    # THIS IS THE COMBINED FEED WITH SORTING ALGORITHM IMPLEMENTED!!
+    @combined = (@discounts + @coupons).sort_by {|record| record.created_at}
   end
 end
