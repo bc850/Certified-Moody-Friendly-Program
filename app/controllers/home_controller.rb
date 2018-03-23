dd201a357030c6ed813259542f360d628a10775e
Brandon Corbett <bmcorbet@valdosta.edu>
2018-03-23 11:13:23 -0400

    reset home_controller and favorite model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @discounts = Discount.all
-    @coupons = Coupon.all
-    @events = Event.all
+    #@discounts = Discount.all
+    #@coupons = Coupon.all
+    #@events = Event.all
 
     # THIS IS THE COMBINED FEED WITH SORTING ALGORITHM IMPLEMENTED!!
-    @combined = (@discounts + @coupons + @events).sort_by(&:created_at).reverse
+    #@combined = (@discounts + @coupons + @events).sort_by(&:created_at).reverse
   end
 end
