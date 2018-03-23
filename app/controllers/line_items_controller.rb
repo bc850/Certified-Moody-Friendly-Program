9c81cebb65439f1d615714a46ebfd770ddbb1a25
amandavsu2018 <amandavsu2018@gmail.com>
2018-03-23 15:22:29 -0400

    Got favorites button to work!

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,4 +1,6 @@
 class LineItemsController < ApplicationController
+  include CurrentFavorite
+  before_action :set_favorite, only: [:create]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
@@ -24,11 +26,12 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @line_item = LineItem.new(line_item_params)
+    offer = Offer.find(params[:offer_id])
+    @line_item = @favorite.line_items.build(offer: offer)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.favorite, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
