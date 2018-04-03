174494cb5574d01525aee87367f83c84d302d8c7
amandavsu2018 <amandavsu2018@gmail.com>
2018-04-03 17:43:44 -0400

    removed format json

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,9 +3,7 @@ class SearchesController < ApplicationController
   before_action :set_favorite
 
   def index
-    respond_to do |format|
-         format.html { }
-         format.json {render json: @offers}
-    end
+    @offers = Offer.all
+    @businesses = Business.all
   end
 end
