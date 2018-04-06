1d8b82351fedea25aa305f530cbb9363032fc49d
Brandon Corbett <bmcorbet@valdosta.edu>
2018-04-05 23:59:51 -0400

    added simple form for navigation, attr_accessor methods in User model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,24 @@ module ApplicationHelper
       html
     end
   end
+
+  ### ADDING HELPER METHODS FOR DEVISE LOGIN
+  ### OUTSIDE OF DEVISE CONTROLLER
+  #:resource_name, :resource, :devise_mapping, :resource_class
+
+  def resource_name
+    :account
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_class
+    User
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:account]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_one :account, as: :accountable
+  attr_accessor :email, :remember_me
 end
