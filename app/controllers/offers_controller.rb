57e2b016454ccd0229e649692344b845916126c2
amandavsu2018 <amandavsu2018@gmail.com>
2018-04-10 20:36:24 -0400

    fixed search; issue was authorization

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -11,32 +11,53 @@ end
   # GET /offers
   # GET /offers.json
   def index
-
-#  @offers = Offer.all
-#    if (current_account.accountable_type == "User")
-#      @offers = Offer.all
-#      respond_to do |format|
-#          format.html { }
-#          format.json {render json: Offer.order(sort_by + ' ' + order)}
-#        end
-#    else
-#      if (params[:business_id])
-#        @business = Business.find(params[:business_id])
-#        @offers = @business.offers
-#      end
-#    end
-#    authorize Offer
-#    @offers = policy_scope(Offer)
-    if (params[:business_id])
-      @business = Business.find(params[:business_id])
-      @offers = @business.offers
-    else
+=begin
+  @offers = Offer.all
+    if (current_account.accountable_type == "User")
       @offers = Offer.all
-      @offers = @offers.order('created_at asc').page params[:page]
+      respond_to do |format|
+          format.html { }
+         format.json {render json: Offer.order(sort_by + ' ' + order)}
+        end
+   else
+      if (params[:business_id])
+        @business = Business.find(params[:business_id])
+       @offers = @business.offers
+    end
+    end
+    authorize Offer
+    @offers = policy_scope(Offer)
+
+    @offers = Offer.all
+    if (current_account.accountable_type == "User")
+      @offers = Offer.all
+      respond_to do |format|
+          format.html { }
+          format.json {render json: Offer.order(sort_by + ' ' + order)}
+        end
+    else
+      if (params[:business_id])
+        @business = Business.find(params[:business_id])
+        @offers = @business.offers
+      end
     end
     authorize Offer
     @offers = policy_scope(Offer)
     @offers = @offers.order('created_at asc').page params[:page]
+=end
+if (params[:business_id])
+    @business = Business.find(params[:business_id])
+    @offers = @business.offers
+  else
+    @offers = Offer.all
+    respond_to do |format|
+             format.html { }
+             format.json {render json: Offer.order(sort_by + ' ' + order)}
+    end
+  end
+
+  authorize Offer
+  @offers = policy_scope(Offer)
   end
 
   # GET /offers/1
