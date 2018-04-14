cdbc1added770c97082cbaa324672e9e780e8835
amandavsu2018 <amandavsu2018@gmail.com>
2018-04-13 21:23:03 -0400

    added end date validator

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,9 +4,20 @@ class Offer < ApplicationRecord
   belongs_to :business
   has_many :line_items
   validates :name, :category, :description, :location, :offering_type, presence: true
-	validates :img_url, allow_blank: true, format: {
+  validates :img_url, allow_blank: true, format: {
 		with:
 		%r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
+
+  validates_presence_of :start_date, :end_date
+  validate :end_date_is_after_start_date
+
+private
+
+def end_date_is_after_start_date
+  if end_date < start_date
+    errors.add(:end_date, "cannot be before the start date")
+  end
+end
 end
