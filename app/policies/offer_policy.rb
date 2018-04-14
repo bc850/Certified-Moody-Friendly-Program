e88a500bde31d8226c05f0f26c71152b8a12cb76
Brandon Corbett <bmcorbet@valdosta.edu>
2018-04-13 20:10:58 -0400

    updated offer policy, businesses/user can all see coupons when clicked

--- app/policies/offer_policy.rb
+++ app/policies/offer_policy.rb
@@ -10,7 +10,8 @@ class OfferPolicy
 	end
 	def show?
 		@current_account == @offer.business.account
-		current_account.accountable_type == "User"
+		#current_account.accountable_type == "User"
+		@current_account == @current_account
 	end
 	def new?
 		current_account.accountable_type == "Business"
