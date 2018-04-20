c3ad65e3815026ef6ca15cf451ff77c85d16765e
amandavsu2018 <amandavsu2018@gmail.com>
2018-04-20 16:32:21 -0400

    Revert "saving but doesn't work"
    This reverts commit a098207c448b2310fcaf6da6a3efc0cd0c58bd1b.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_one :account, as: :accountable
-  BRANCH_TYPES=["Air Force", "Army", "Coast Guard", "Marine Corps", "Navy"]
   attr_accessor :email, :remember_me, :mil_branch
+  BRANCH_TYPES=["Air Force", "Army", "Coast Guard", "Marine Corps", "Navy"]
 end
