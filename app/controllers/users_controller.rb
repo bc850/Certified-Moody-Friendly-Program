a098207c448b2310fcaf6da6a3efc0cd0c58bd1b
amandavsu2018 <amandavsu2018@gmail.com>
2018-04-20 15:22:21 -0400

    saving but doesn't work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,52 +6,24 @@ def pundit_user
   current_account
 end
 
-
   # GET /users
   # GET /users.json
   def index
     @users = User.all
   end
-=begin
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
-  end
-
-#=end
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-=end
   # GET /users/1/edit
   def edit
     authorize @user
   end
 
-=begin
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-=end
-
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
     authorize @user
+
+    @user.mil_branch = current_account.accountable.mil_branch
+
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to feed_index_path, notice: "The profile of #{@user.name} was successfully updated." }
@@ -62,18 +34,6 @@ end
       end
     end
   end
-
-=begin
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-=end
 
   private
     # Use callbacks to share common setup or constraints between actions.
