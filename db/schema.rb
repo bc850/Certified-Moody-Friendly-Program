77370de12f87dbef33c97723ce9cd3c8d43f8e94
Brandon Corbett <bmcorbet@valdosta.edu>
2018-06-21 00:15:21 -0400

    combine items in cart migration made

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180621034730) do
+ActiveRecord::Schema.define(version: 20180621040105) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
