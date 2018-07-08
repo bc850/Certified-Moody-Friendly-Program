94229b6e63a71878dda1358b154273cfae66720d
Brandon Corbett <bmcorbet@valdosta.edu>
2018-07-07 23:27:46 -0400

    added set_active and expiration columns to advertisements table

--- db/migrate/20180620012203_create_advertisements.rb
+++ db/migrate/20180620012203_create_advertisements.rb
@@ -6,6 +6,8 @@ class CreateAdvertisements < ActiveRecord::Migration[5.1]
       t.string :ad_type, default: 'Banner'
       t.string :image_url
       t.string :web_url
+      t.string :set_active, default: 'false'
+      t.date :expiration
 
       t.timestamps
     end
